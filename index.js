30413893926bd74989e99a315b0a859e1e006be9
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-04-04 17:39:59 -0300

    add regex

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
+function extraLinks(texto) {
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+
+    const resultados = [];
+
+    let temp;
+
+    while((temp = regex.exec(texto)) !== null) {
+        resultados.push({ [temp[1]]: temp[2] })
+    }
+
+    return resultados;
+}
+
 function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho.'));
 }
@@ -9,31 +23,12 @@ async function pegaArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8';
     try {
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        console.log(chalk.green(texto));
+        console.log(extraLinks(texto));
     } catch(erro) {
         trataErro(erro);
     } finally {
         console.log(chalk.yellow('Programa finalizado!!!'))
     }
 }
-
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = 'utf-8';
-
-//     fs.promises
-//         .readFile(caminhoDoArquivo, encoding)
-//         .then((texto) => console.log(chalk.green(texto)))
-//         .catch((erro) => trataErro(erro))
-// }
-
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = 'utf-8';
-//     fs.readFile(caminhoDoArquivo, encoding, (erro, data) => {
-//         if(erro) {
-//             trataErro(erro);
-//         }
-//         console.log(chalk.green(data));
-//     });
-// }
 
 pegaArquivo('./arquivos/texto1.md');
