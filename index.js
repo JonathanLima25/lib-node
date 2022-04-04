254bf3d936b189e0b0029d085c8a90ef1d441626
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-04-04 15:06:11 -0300

    add promises

---
+++ index.js
@@ -0,0 +1,27 @@
+const chalk = require('chalk');
+const fs = require('fs');
+
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho.'));
+}
+
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+
+    fs.promises
+        .readFile(caminhoDoArquivo, encoding)
+        .then((texto) => console.log(chalk.green(texto)))
+        .catch((erro) => trataErro(erro))
+}
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, data) => {
+//         if(erro) {
+//             trataErro(erro);
+//         }
+//         console.log(chalk.green(data));
+//     });
+// }
+
+pegaArquivo('./arquivos/texto1.md');
