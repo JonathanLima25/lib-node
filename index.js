96e38c705a37148093f5b7be86da20bcb5333e41
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-04-04 15:13:41 -0300

    add async e await

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho.'));
 }
 
-function pegaArquivo(caminhoDoArquivo) {
+async function pegaArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8';
-
-    fs.promises
-        .readFile(caminhoDoArquivo, encoding)
-        .then((texto) => console.log(chalk.green(texto)))
-        .catch((erro) => trataErro(erro))
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(chalk.green(texto));
 }
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+
+//     fs.promises
+//         .readFile(caminhoDoArquivo, encoding)
+//         .then((texto) => console.log(chalk.green(texto)))
+//         .catch((erro) => trataErro(erro))
+// }
 
 // function pegaArquivo(caminhoDoArquivo) {
 //     const encoding = 'utf-8';
