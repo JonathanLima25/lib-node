d5a7ea63ff07e30f9bf56e857c01e7976840dd87
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-04-05 17:52:15 -0300

    install node-fetch

--- cli.js
+++ cli.js
@@ -7,7 +7,7 @@ const caminho = process.argv;
 async function processaTexto(caminhoDeArquivo) {
     const resultado = await pegaArquivo(caminhoDeArquivo[2]);
     if (caminho[3] === 'validar') {
-        console.log(chalk.yellow('links validados'), validaURL(resultado));
+        console.log(chalk.yellow('links validados'), await validaURL(resultado));
     } else {
         console.log(chalk.yellow('lista de links'), resultado);
     }
